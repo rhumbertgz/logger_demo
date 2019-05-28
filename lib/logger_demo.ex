aaf7160392b61297088b9706438c2a55a3c55f62
Humberto Rodriguez Avila <rhumbert@vub.ac.be>
2019-05-28 14:20:34 +0200

    Add demo

---
+++ lib/logger_demo.ex
@@ -0,0 +1,18 @@
+defmodule LoggerDemo do
+  @moduledoc """
+  Custom Logger Formatter Demo
+  """
+  require Logger
+
+  def test do
+    Logger.info "Demo"
+    Logger.info "Basic" , output: :inner
+
+    Logger.debug "Demo"
+    Logger.debug "Basic" , output: :inner
+
+    Logger.error "Demo"
+    Logger.error "Basic" , output: :inner
+
+  end
+end
